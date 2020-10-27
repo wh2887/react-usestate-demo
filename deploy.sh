f4d39c4c049cc4f7b2445e8062c27967fe80bf27
McCallWang <13567272790@163.com>
2020-10-27 21:51:18 +0800

    改变state为数组即可解决，有下标bug

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "使用两个myUseState发现冲突，后者覆盖前者。这是bug。"
+git commit -m "改变state为数组即可解决，有下标bug"
 git push git@github.com:wh2887/react-usestate-demo.git -f 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-let _state
+let _state = []
+let index = 0
 function myUseState(initState) {
-  _state = _state === undefined ? initState : _state
-  console.log(_state)
+  _state[index] = _state[index] === undefined ? initState : _state[index]
+  console.log(_state[index])
   function setState(newState) {
-    _state = newState
+    // 发现此时这边的 index 会有问题。因为下一次应该是 index +1
+    _state[index] = newState
     render()
   }
-  return [_state, setState]
+  // index += 1 这边 +1 了 那么下面 return 的就不是当前的一个，而是下一个了。 单页不可能放到return后面。所以这条路不对啊。
+  // 需要一个中介，保存当时的index，然后修改index，但是当前的index并不会丢失。 
+  return [_state[index], setState]
 }
 
 const render = () => {
