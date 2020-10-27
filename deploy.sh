87fc55c0a1e79b04c80ad16bf7b612569316274a
McCallWang <13567272790@163.com>
2020-10-27 22:01:47 +0800

    下标一直在累加，并没有在下一次re-render App 之前 初始化（重置index）

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "改变state为数组即可解决，有下标bug"
+git commit -m "下标一直在累加，并没有在下一次re-render App 之前 初始化（重置index）"
 git push git@github.com:wh2887/react-usestate-demo.git -f 
--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ import ReactDOM from 'react-dom';
 let _state = []
 let index = 0
 function myUseState(initState) {
-  _state[index] = _state[index] === undefined ? initState : _state[index]
-  console.log(_state[index])
+  const currentIndex = index
+  console.log('currentIndex')
+  console.log(currentIndex)
+  _state[currentIndex] = _state[currentIndex] === undefined ? initState : _state[currentIndex]
+  console.log(_state)
+  index += 1
   function setState(newState) {
     // 发现此时这边的 index 会有问题。因为下一次应该是 index +1
-    _state[index] = newState
+    _state[currentIndex] = newState
     render()
   }
   // index += 1 这边 +1 了 那么下面 return 的就不是当前的一个，而是下一个了。 单页不可能放到return后面。所以这条路不对啊。
   // 需要一个中介，保存当时的index，然后修改index，但是当前的index并不会丢失。 
-  return [_state[index], setState]
+  return [_state[currentIndex], setState]
 }
 
 const render = () => {
@@ -24,7 +28,7 @@ const render = () => {
 const App = () => {
   // 原 useState: const [n,setN] = useState(0)
   const [state, setState] = myUseState(0)
-  const [m, setM] = myUseState(1)
+  const [m, setM] = myUseState(0)
 
 
   return (
